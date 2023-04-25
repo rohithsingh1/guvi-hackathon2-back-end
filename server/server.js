f2207390d09b95c102233b3d9edb6f4f6a9432c0
rohithsingh1 <rohithsinghthakur3@gmail.com>
2023-04-25 19:44:22 +0530

    First commit

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+app.use(express.json());
+
+const userRoute = require("./routes/usersRoute");
+const movieRoute = require("./routes/moviesRoute");
+const theatresRoute = require("./routes/theatresRoute");
+const bookingsRoute = require("./routes/bookingsRoute");
+
+app.use("/api/users",userRoute);
+app.use("/api/movies",movieRoute);
+app.use("/api/theatres", theatresRoute);
+app.use("/api/bookings", bookingsRoute);
+
+
+const port = process.env.PORT || 5000;
+
+const path = require("path");
+__dirname = path.resolve();
+
+
+// render deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+
+app.listen(port,()=>{
+    console.log(`Node JS Server is running on port ${port}`);
+});
+
